62814df5550a253dc1021bb0b0c93ba262814615
Bartlomiej Ciura <bartlomiej.ciura@autentika.pl>
2020-05-26 14:41:47 +0200

    Bugify - content type

--- src/ArticleExtractor.php
+++ src/ArticleExtractor.php
@@ -75,6 +75,10 @@ class ArticleExtractor
 		// First attempt to parse the URL into the structure we want
 		$results = $this->parseViaReadability($url);
 
+        if (isset($results['error'])) {
+            return $results;
+        }
+
 		// If we don't see what we want, try our other method
 		if ($results['text'] == null) {
 			$results = $this->parseViaGooseOrCustom($url);
@@ -171,6 +175,12 @@ class ArticleExtractor
             curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36');
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             $html = curl_exec($ch);
+
+            $contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+
+            if ($contentType === 'application/pdf') {
+                return ['error' => 'incorrect_content_type'];
+            }
 
 			$readability->parse($html);
 			$title = $readability->getTitle();
